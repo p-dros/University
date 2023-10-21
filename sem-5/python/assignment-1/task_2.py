b1d5b6846bd786ca1449b25d7dc6cf1c0c685634
Piotr Droś <piotr.dros01@gmail.com>
2023-10-21 14:47:28 +0200

    assigment_1 - task_2

---
+++ sem-5/python/assignment-1/task_2.py
@@ -0,0 +1,23 @@
+START_NUM = 98
+LENGTH = 15
+
+max_num = START_NUM + LENGTH - 1
+max_num_len = len(str(max_num))
+DIGITS_COUNT = 2 + max_num_len
+
+start_num_digit_count = len(str(START_NUM))
+measure = ""
+
+measure += f"{" " * (start_num_digit_count - 1)}|"
+for num in range(LENGTH - 1):
+    measure += f"{"." * DIGITS_COUNT}|"
+measure += "\n"
+
+space_count = 0
+for num in range(START_NUM, LENGTH + START_NUM):
+    measure += f"{' ' * space_count}{num}"
+
+    digit_count = len(str(num + 1))
+    space_count = DIGITS_COUNT + 1 - digit_count
+
+print(measure)
